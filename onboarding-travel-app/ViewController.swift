a695bf1f47f219eec7eb1d1c393ad30eea3374cd
SergeyGolenko <1989sergiotay@gmail.com>
2021-01-06 17:11:29 +0200

    add all outlets and connect with ViewController

--- onboarding-travel-app/ViewController.swift
+++ onboarding-travel-app/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var bgImageView: UIImageView!
+    @IBOutlet weak var darkView: UIView!
+    @IBOutlet weak var pageControll: UIPageControl!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    
+    
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
